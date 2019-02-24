354ba90f0f7e71bc66b199e14ccbbb13b1e70814
Michał Lipek <michal@lipek.net>
2019-02-24 22:07:41 +0100

    Gigasecond

--- kotlin/gigasecond/src/main/kotlin/Gigasecond.kt
+++ kotlin/gigasecond/src/main/kotlin/Gigasecond.kt
@@ -2,7 +2,7 @@ import java.time.LocalDate
 import java.time.LocalDateTime
 
 class Gigasecond(inputDateTime: LocalDateTime) {
-    val date = inputDateTime.plusSeconds(1000000000)!!
+    val date: LocalDateTime = inputDateTime.plusSeconds(1000000000)
 
     constructor(inputDate: LocalDate): this(inputDate.atStartOfDay())
 }
