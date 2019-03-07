2be161f7194250d2643c984c4b6853e0d55bb492
Michał Lipek <michal@lipek.net>
2019-03-07 23:20:27 +0100

    Space age

--- kotlin/space-age/src/main/kotlin/SpaceAge.kt
+++ kotlin/space-age/src/main/kotlin/SpaceAge.kt
@@ -4,7 +4,7 @@ private const val SECONDS_IN_EARTH_YEAR = 31_557_600.0
 
 class SpaceAge (private val seconds: Long) {
 
-    enum class Planet(val ratio: Double) {
+    enum class SolarYear(private val ratio: Double) {
         Earth(1.0),
         Mercury(0.2408467),
         Venus(0.61519726),
@@ -14,17 +14,17 @@ class SpaceAge (private val seconds: Long) {
         Uranus(84.016846),
         Neptune(164.79132);
 
-        fun ratio() = SECONDS_IN_EARTH_YEAR * ratio
+        fun ratioToToEarth() = SECONDS_IN_EARTH_YEAR * ratio
     }
 
-    fun onEarth(): Double = round(seconds / Planet.Earth.ratio())
-    fun onMercury(): Double = round(seconds / Planet.Mercury.ratio())
-    fun onVenus(): Double = round(seconds / Planet.Venus.ratio())
-    fun onMars(): Double = round(seconds / Planet.Mars.ratio())
-    fun onJupiter(): Double = round(seconds / Planet.Jupiter.ratio())
-    fun onSaturn(): Double = round(seconds / Planet.Saturn.ratio())
-    fun onUranus(): Double = round(seconds / Planet.Uranus.ratio())
-    fun onNeptune(): Double = round(seconds / Planet.Neptune.ratio())
+    fun onEarth(): Double = round(seconds / SolarYear.Earth.ratioToToEarth())
+    fun onMercury(): Double = round(seconds / SolarYear.Mercury.ratioToToEarth())
+    fun onVenus(): Double = round(seconds / SolarYear.Venus.ratioToToEarth())
+    fun onMars(): Double = round(seconds / SolarYear.Mars.ratioToToEarth())
+    fun onJupiter(): Double = round(seconds / SolarYear.Jupiter.ratioToToEarth())
+    fun onSaturn(): Double = round(seconds / SolarYear.Saturn.ratioToToEarth())
+    fun onUranus(): Double = round(seconds / SolarYear.Uranus.ratioToToEarth())
+    fun onNeptune(): Double = round(seconds / SolarYear.Neptune.ratioToToEarth())
 
     private fun round(number: Double): Double = (DecimalFormat("#.##")).format(number).toDouble()
 }
